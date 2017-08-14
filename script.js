53894b0e976cf1d6ca40e83317995f87078db723
Aoreyus <30877980+Aoreyus@users.noreply.github.com>
2017-08-14 10:51:51 -1000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+function sayHello() {
+//I am creating a function called hello
+ var userName = prompt("Enter your user name")
+//Creat a variable called userName and stores the users information
+var nickName =prompt("Enter your nick name")
+var lastName = prompt("Enter your lastname")
+var firstName = prompt("Enter your first name")
+ window.alert("okay" + " " + userName + " " + firstName + " " +lastName + " " + nickName);
+
+//Pop up a window that says hello to the user
+ 
+
+
+}
